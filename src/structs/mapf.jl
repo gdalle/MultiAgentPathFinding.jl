7c22dc9399c34c0d5304ea002fcf5f2c007bd722
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2022-09-05 13:22:52 +0200

    Add swapping conflicts by default

--- src/structs/mapf.jl
+++ src/structs/mapf.jl
@@ -100,7 +100,9 @@ function MAPF(
     arrivals;
     departure_times=fill(1, length(departures)),
     vertex_conflicts=Dict(v => [v] for v in vertices(g)),
-    edge_conflicts=Dict((src(ed), dst(ed)) => [(dst(ed), src(ed))] for ed in edges(g)),
+    edge_conflicts=Dict(
+        (src(ed), dst(ed)) => [(src(ed), dst(ed)), (dst(ed), src(ed))] for ed in edges(g)
+    ),
     stay_at_arrival=true,
 ) where {G}
     edge_indices, edge_colptr, edge_rowval, edge_weights_vec = build_edge_data(g)
