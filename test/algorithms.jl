2e27c4e68d417adfa0d919b64f1a1c7acfa96f10
Simone Carlo Surace <simsurace@gmx.net>
2025-04-10 13:58:02 +0200

    Add regression test for issue #20

--- test/algorithms.jl
+++ test/algorithms.jl
@@ -8,36 +8,45 @@ Random.seed!(63)
 
 show_progress = get(ENV, "CI", "false") == "false"
 
-L = 20
-g = Graphs.grid([L, L])
+@testset "Grid" begin
+    L = 20
+    g = Graphs.grid([L, L])
 
-A = 20
-departures = 1:A;
-arrivals = (nv(g) + 1) .- (1:A);
+    A = 20
+    departures = 1:A
+    arrivals = (nv(g) + 1) .- (1:A)
 
-mapf = MAPF(g; departures, arrivals);
+    mapf = MAPF(g; departures, arrivals)
 
-sol_indep = independent_dijkstra(mapf; show_progress);
-sol_coop = cooperative_astar(mapf; show_progress);
-sol_os, stats_os = optimality_search(mapf; show_progress);
-sol_fs, stats_fs = feasibility_search(mapf; show_progress);
-sol_ds, stats_ds = double_search(mapf; show_progress);
+    sol_indep = independent_dijkstra(mapf; show_progress)
+    sol_coop = cooperative_astar(mapf; show_progress)
+    sol_os, stats_os = optimality_search(mapf; show_progress)
+    sol_fs, stats_fs = feasibility_search(mapf; show_progress)
+    sol_ds, stats_ds = double_search(mapf; show_progress)
 
-@test !is_feasible(sol_indep, mapf; verbose=false)
-@test is_feasible(sol_coop, mapf, verbose=true)
-@test is_feasible(sol_os, mapf, verbose=true)
-@test is_feasible(sol_fs, mapf, verbose=true)
-@test is_feasible(sol_ds, mapf, verbose=true)
+    @test !is_feasible(sol_indep, mapf; verbose=false)
+    @test is_feasible(sol_coop, mapf, verbose=true)
+    @test is_feasible(sol_os, mapf, verbose=true)
+    @test is_feasible(sol_fs, mapf, verbose=true)
+    @test is_feasible(sol_ds, mapf, verbose=true)
 
-f_indep = solution_cost(sol_indep, mapf)
-f_coop = solution_cost(sol_coop, mapf)
-f_fs = solution_cost(sol_fs, mapf)
-f_os = solution_cost(sol_os, mapf)
-f_ds = solution_cost(sol_ds, mapf)
+    f_indep = solution_cost(sol_indep, mapf)
+    f_coop = solution_cost(sol_coop, mapf)
+    f_fs = solution_cost(sol_fs, mapf)
+    f_os = solution_cost(sol_os, mapf)
+    f_ds = solution_cost(sol_ds, mapf)
 
-stats_os
-stats_fs
-stats_ds
+    stats_os
+    stats_fs
+    stats_ds
 
-@test f_indep <= f_os <= f_coop
-@test f_indep <= f_ds <= f_fs
+    @test f_indep <= f_os <= f_coop
+    @test f_indep <= f_ds <= f_fs
+end
+
+@testset "issue #20" begin
+    g = Graph(2)
+    problem = MAPF(g, weights(g); departures=[1], arrivals=[2], departure_times=[0])
+    solution, _ = optimality_search(problem)
+    @test !is_feasible(solution, problem)
+end
