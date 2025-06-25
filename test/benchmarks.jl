57077d5b12d0763a93b43b77437b5f433bdf974a
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-06-25 18:24:23 +0200

    Refic

--- test/benchmarks.jl
+++ test/benchmarks.jl
@@ -73,6 +73,6 @@ end
     A[2, 3] = 1
     departure_coords = [(1, 1)]
     arrival_coords = [(10, 10)]
-    mapf = MAPF(A, departure_coords, departure_coords)
+    mapf = MAPF(A, departure_coords, arrival_coords)
     @test nv(mapf.g) == 99
 end
