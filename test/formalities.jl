7784d45f7de6abe03310f5d4df114b80bd1f3f8f
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-11-25 07:22:30 +0100

    Fix formatting (#40)

--- test/formalities.jl
+++ test/formalities.jl
@@ -5,10 +5,10 @@ using MultiAgentPathFinding
 using Test
 
 @testset "Code quality (Aqua)" begin
-    Aqua.test_all(MultiAgentPathFinding; ambiguities = false, undocumented_names = true)
+    Aqua.test_all(MultiAgentPathFinding; ambiguities=false, undocumented_names=true)
 end
 @testset "Code formatting (JuliaFormatter)" begin
-    @test format(MultiAgentPathFinding; verbose = false, overwrite = false)
+    @test format(MultiAgentPathFinding; verbose=false, overwrite=false)
 end
 @testset "Code linting (JET)" begin
     #TODO: toggle
