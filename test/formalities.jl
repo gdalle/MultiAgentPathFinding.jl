5304fb71d043c0213cc263e987a250fc50a01a9f
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-11-25 07:16:24 +0100

    Disable

--- test/formalities.jl
+++ test/formalities.jl
@@ -5,11 +5,12 @@ using MultiAgentPathFinding
 using Test
 
 @testset "Code quality (Aqua)" begin
-    Aqua.test_all(MultiAgentPathFinding; ambiguities=false, undocumented_names=true)
+    Aqua.test_all(MultiAgentPathFinding; ambiguities = false, undocumented_names = true)
 end
 @testset "Code formatting (JuliaFormatter)" begin
-    @test format(MultiAgentPathFinding; verbose=false, overwrite=false)
+    @test format(MultiAgentPathFinding; verbose = false, overwrite = false)
 end
 @testset "Code linting (JET)" begin
-    JET.test_package(MultiAgentPathFinding; target_modules=(MultiAgentPathFinding,))
+    #TODO: toggle
+    # JET.test_package(MultiAgentPathFinding; target_modules=(MultiAgentPathFinding,))
 end
